7e272e72527c24643fef1ba40a884afc45901b10
admin <admin@admins-MacBook-Pro.local>
2026-03-29 15:30:28 +0200

    security: remove secrets from providers.tf and add gitignore

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
-      version = "2.9.14"
+      version = "3.0.1-rc4"
     }
   }
 }
 
 provider "proxmox" {
   pm_api_url          = "https://62.210.89.8:8006/api2/json"
-  pm_api_token_id     = "root@pam!terraform"
-  pm_api_token_secret = "2d5f17bb-594c-42db-b340-07c094df6384"
+  pm_api_token_id     = var.proxmox_token_secretid
+  pm_api_token_secret = var.proxmox_token_secret
   pm_tls_insecure     = true
 }
--- terraform/proxmox/variables.tf
+++ terraform/proxmox/variables.tf
@@ -13,3 +13,14 @@ variable "storage" {
 variable "ssh_public_key" {
   default = "~/.ssh/id_rsa.pub"
 }
+
+
+variable "proxmox_token_secretid" {
+  description = "Token ID Proxmox API"
+  sensitive   = true
+}
+
+variable "proxmox_token_secret" {
+  description = "Token secret Proxmox API"
+  sensitive   = true
+}
